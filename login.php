8b4398642c009dd97ecbcece543267cf1d053de3
filippo-ceid <filippo@ceid.upatras.gr>
2014-04-19 19:24:58 +0300

    Ellhnika

--- login.php
+++ login.php
@@ -15,10 +15,10 @@
     <div id="top_panel">
 		<div class="login_field">
 			<form action="login.php" method="POST">
-				Email<br>
+				Διεύθυνση Email:<br>
 				<input type="text" name="email" value="<?php if(isset($_POST['email'])) { echo $_POST['email']; } ?>"/>
 				<div class="error_login_field"><?php echo $email_error_msg ?></div><br>
-				<br>Password<br>
+				<br>Κωδικός Ασφαλείας:<br>
 				<input type="password" name="password" />
 				<div class="error_login_field"><?php echo $pass_error_msg ?></div><br>
 				<br><input type="submit" name="submit" value="Login" />
