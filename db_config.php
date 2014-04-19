361cd1262428c84dc8803d4fdcd1484b628210cb
filippo-ceid <filippo@ceid.upatras.gr>
2014-04-19 19:26:32 +0300

    Select edw kai ellhnika

--- db_config.php
+++ db_config.php
@@ -4,11 +4,14 @@
 	DEFINE ('DB_PASSWORD', '123456');
 	DEFINE ('DB_HOST', 'localhost');
 	DEFINE ('DB_NAME', 'FMC_DB');
+	DEFINE ('SITE_NAME','FixMyCity');
+	DEFINE ('BASE_URL', 'http://www.fixmycity.gr/');
+	DEFINE ('ADM_EMAIL', 'filippo@ceid.upatras.gr');
 
 	// Make the connection:
-	$dbhandle = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD);
-
-	if (!$dbhandle) {
-		trigger_error ('Could not connect to MySQL: ' . mysql_connect_error() );
-	}
+	$dbhandle = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
+	
+	mysql_select_db(DB_NAME,$dbhandle) or die("Could not select " . DB_NAME);
+	
+	mysql_query('SET CHARACTER SET utf8') or die(mysql_error());
 ?>
