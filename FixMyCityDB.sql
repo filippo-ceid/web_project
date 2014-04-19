5747605c27344cd2ee5bfa1294c7214238ddc2d3
filippo-ceid <filippo@ceid.upatras.gr>
2014-04-19 19:22:31 +0300

    Allages sth dhmiourgia ths Vashs

--- FixMyCityDB.sql
+++ FixMyCityDB.sql
@@ -3,35 +3,27 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 -- Database: FMC_DB (FixMyCity)
-CREATE database FMC_DB;
-SHOW databases;
+CREATE DATABASE FMC_DB CHARACTER SET utf8 COLLATE utf8_general_ci;
+SHOW DATABASES;
 USE FMC_DB;
 
 -- Table users for login
-CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS users  (
+  user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(50) NOT NULL,
   password VARCHAR(128) NOT NULL,
-  user_type ENUM('user','admin') NOT NULL
+  user_level ENUM('simple','admin') NOT NULL,
+  firstname VARCHAR(20),
+  lastname VARCHAR(20),
+  phone VARCHAR(10)
 ) ENGINE=InnoDB;
 
-INSERT INTO users (email, password, user_type) VALUES
-('admin@', 'admin', 'admin'),
-('filippo@', '1234', 'user'),
-('kalexio@', '1234', 'user');
+INSERT INTO users (email, password, user_level, firstname, lastname, phone) VALUES
+('admin@', 'admin', 'admin', 'admin', '', '');
 
 SELECT * FROM users;
-
-------------------------------------------------------
-
--- Table profiles
-CREATE TABLE IF NOT EXISTS profiles (
-  id INT(11) NOT NULL PRIMARY KEY,
-  fname VARCHAR(100),
-  lname VARCHAR(100),
-  phone INT(10),
-) ENGINE=InnoDB;
-
-INSERT INTO profiles (id, fname, lname, phone) VALUES
-(1, 'Admin', '', 2105478952),
-(2, 'Filippos', 'F', 2631051452);
+DELETE FROM users WHERE user_id  = <number>;
+TRUNCATE TABLE users;
+DROP DATABASE FMC_DB;
+DROP TABLE users;
+SHOW TABLES;
