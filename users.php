8c316640f714a9e159d9688259a806dac87a130f
filippo-ceid <filippo@ceid.upatras.gr>
2014-04-22 04:24:42 +0300

    Diaxeirisi logariasmwn apo ton Admin

---
+++ users.php
@@ -0,0 +1,17 @@
+<?php
+    // Start output buffering:
+	ob_start();
+	// Initialize a session:
+	session_start();
+	include 'header.html';
+	$tab=3;
+	require  "menu.php";
+?>   
+    <div id="top_panel">
+		<div class="under_con_image">
+            		<img src="images/under-construction.png"/></img>
+        </div>
+    </div> <!-- end of top panel -->
+<?php
+	include 'footer.html';
+?>
