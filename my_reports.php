d7d2c9cab67bd760d5188a984decd49f805a52e9
filippo-ceid <filippo@ceid.upatras.gr>
2014-05-23 21:24:53 +0300

    nea anafora

--- my_reports.php
+++ my_reports.php
@@ -1,10 +1,14 @@
 <?php
+	
 	include 'header.html';
 	require "new_report.php";
 	$tab=2;
 	require  "menu.php";
 ?>   
-	
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=true"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
+	<script type="text/javascript" src="new_marker_map.js"></script>
     <div id="top_panel">
 		<div class="report_field">
 			<div class="myreport_report"><?php echo $report ?></div><br>
@@ -12,28 +16,24 @@
 				<table>
 					<tr>
 					  <td>
-						 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=true"></script>
-						 <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-						 <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.1.1.min.js"></script>
-						 <script type="text/javascript" src="new_marker_map.js"></script>
 						 <div id="new_marker_map_canvas"></div>
 					  </td>
 					</tr>
 					<tr>
 					  <td>
-						  <br>Κατηγορία:<br>
+						  <br>Κατηγορία:<div class="error_rep_field"> *<?php echo $category_error ?></div><br>
 						  <select name="category">
 							  <option value="default">-Επιλέξτε-</option>
 							  <option value="road">Road</option>
 							  <option value="sky">Sky</option>
-						  </select> <div class="error_category_field">*<?php echo $category_error ?></div>
+						  </select>
 						  <br>
 					  </td>
 					</tr>
 					<tr>
 					  <td>
-						  <br>Περιγραφή:<br>
-						  <textarea name="description" maxlength="500" cols="76" rows="10"><?php if(isset($description)){echo $description;}?></textarea>
+						  <br>Περιγραφή:<div class="error_rep_field"> *<?php echo $description_error ?></div><br>
+						  <textarea name="description" maxlength="500" cols="76" rows="10"><?php if(isset($_POST['description'])&&($_POST['category']=="default")){echo $_POST['description'];}?></textarea>
 					  </td>
 					</tr>
 				</table> 
@@ -42,9 +42,17 @@
 					<input type="submit" name="submit" value="Submit"/>
 				</div>
 			</form>
-			<div class="reg_image">
-			</div>
         </div>
+        <div class="old_reports">
+			<ul>
+				<li>Page 1</li>
+				<li>Page 2</li>
+				<li>Page 3</li>
+				<li>Page 4</li>
+				<li>Page 5</li>
+				<li>Page 6</li>
+			</ul>
+		</div>
     </div> <!-- end of top panel -->
     
 <?php
