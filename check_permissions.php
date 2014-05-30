d967221efc155e0d9ebece371394e678814aeab6
filippo-ceid <filippo@ceid.upatras.gr>
2014-05-30 07:26:47 +0300

    fixes

--- check_permissions.php
+++ check_permissions.php
@@ -8,6 +8,8 @@ function check_simple_permissions($user_id){
 	$query = "SELECT user_level FROM users WHERE user_id = $user_id;";
 	$result = mysqli_query($dbhandle,$query);
 	$row = mysqli_fetch_assoc($result);
+	mysqli_free_result($result);
+	mysqli_close($dbhandle);
 	$user_level = $row['user_level'];
 	if ($user_level != "simple"){
 		$_SESSION = array(); // Destroy the variables.
@@ -22,6 +24,8 @@ function check_admin_permissions($user_id){
 		$query = "SELECT user_level FROM users WHERE user_id = $user_id;";
 		$result = mysqli_query($dbhandle,$query);
 		$row = mysqli_fetch_assoc($result);
+		mysqli_free_result($result);
+		mysqli_close($dbhandle);
 		$user_level = $row['user_level'];
 		if ($user_level != "admin"){
 			$_SESSION = array(); // Destroy the variables.
@@ -35,6 +39,8 @@ function check_permissions($user_id){
 	require "db_config.php";
 	$query = "SELECT user_level FROM users WHERE user_id = $user_id;";
 	$result = mysqli_query($dbhandle,$query);
+	mysqli_free_result($result);
+	mysqli_close($dbhandle);
 	$row = mysqli_fetch_assoc($result);
 	$user_level = $row['user_level'];
 	if ($user_level != "admin" && $user_level != "simple"){
