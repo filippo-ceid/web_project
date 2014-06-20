e5e4883174c1302a2a1720ff01d4b5c960cb843f
filippo-ceid <filippo@ceid.upatras.gr>
2014-06-20 17:50:11 +0300

    orismos ip kai port tis php

--- db_config.php
+++ db_config.php
@@ -1,8 +1,9 @@
 <?php
-	$DB_USER = 'root';
-	$DB_PASSWORD =  '123456';
-	$DB_HOST =  'localhost';
-	$DB_NAME = 'FMC_DB';
+	$DB_USER = 'root';	//mysql user
+	$DB_PASSWORD = '123456'; //mysql password
+	$DB_HOST = 'localhost'; //<server_ip>:<mysql_port>
+	$DB_NAME = 'FMC_DB'; //mysql database
+	$PHP_HOST = 'localhost'; //<server_ip>:<php_port>
 
 	// Make the connection:
 	$dbhandle = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD,$DB_NAME) or die("Failed to connect to MySQL: " . mysql_error());
