eb53a77281a10ee55b47f4aa49e78d2e62eb3464
filippo-ceid <filippo@ceid.upatras.gr>
2014-06-20 17:56:43 +0300

    anoigma index me to marker pou epilegetai

--- rss.php
+++ rss.php
@@ -9,7 +9,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>
 <channel>
 <title>FixMyCity</title>
 <description>Τελευταίες 20 αναφορές</description>
-<link>http://localhost/web_project/rss.php</link>';
+<link>http://'.$PHP_HOST.'/web_project/rss.php</link>';
 
 $query = sprintf("SELECT category, description, datetime, firstname, lastname FROM reports INNER JOIN users on users.user_id = reports.user_id ORDER BY report_id DESC LIMIT 20;");
 $result = mysqli_query($dbhandle,$query);
@@ -17,15 +17,18 @@ if (!$result) {
 		header('HTTP/1.1 500 Error: Could not get reports!'); 
 		exit();
 }
+$report_num = 0;
 
 while ($row = mysqli_fetch_array($result)){
     echo '
        <item>
           <title>'.$row['datetime'].'</title>
           <description><![CDATA[<html><body>Κατηγορία: '.$row['category'].'<br>Περιγραφή:<br>'.$row['description'].'<br>Χρήστης: '.$row['firstname'].' '.$row['lastname'].'</body></html>]]></description>
-          <link>http://localhost/web_project/index.php</link>
+          <link>http://'.$PHP_HOST.'/web_project/index.php?'.$report_num.'</link>
       </item>';
+      $report_num = $report_num +1;
 }
+
 echo '</channel>
 </rss>';
 ?>
