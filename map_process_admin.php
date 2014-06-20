645e358753a31946295b84b6b1d7ac64050c2197
filippo-ceid <filippo@ceid.upatras.gr>
2014-06-20 17:51:16 +0300

    fix sto provlima me ta url

--- map_process_admin.php
+++ map_process_admin.php
@@ -79,21 +79,16 @@ function GetReports($status, $page){
 	$result = mysqli_query($dbhandle,$query);
 	$count_row = mysqli_fetch_assoc($result);
 	$counts = $count_row['COUNT(*)'];
+	
 	if ($page==0){
-		$query = sprintf("SELECT reports.report_id ,category, description, datetime, lat, lng, email, admin_id FROM reports INNER JOIN status on reports.report_id=status.report_id INNER JOIN users on users.user_id = reports.user_id WHERE status.status ='$status' ORDER BY category, datetime DESC;");
+		$page_num = ceil($counts/20);
+		$page_n = 1;
 	}
 	else {
-		$first_report = ($page - 1)*3;
-		$query = sprintf("SELECT * FROM (SELECT reports.report_id ,category, description, datetime, lat, lng, email, admin_id FROM reports INNER JOIN status on reports.report_id=status.report_id INNER JOIN users on users.user_id = reports.user_id WHERE status.status ='$status' ORDER BY datetime DESC  LIMIT $first_report,3) AS T1 ORDER BY category;");
+		$page_num = $page;
+		$page_n = $page;
 	}
 	
-	$result = mysqli_query($dbhandle,$query);
-
-	if (!$result) {  
-		header('HTTP/1.1 500 Error: Could not get reports!'); 
-		exit();
-	} 
-
 	//set document header to text/xml
 	header("Content-type: text/xml"); 
 
@@ -102,39 +97,52 @@ function GetReports($status, $page){
 	$dom->formatOutput = true;
 	$node = $dom->createElement("reports"); //Create new element node
 	$parnode = $dom->appendChild($node); //make the node show up 
-
-	// Iterate through the rows, adding XML nodes for each
-	while ($row = mysqli_fetch_assoc($result)){
-		$node = $dom->createElement("report");
-		$newnode = $parnode->appendChild($node);
-		$newnode->setAttribute("category", $row['category']);
-		$newnode->setAttribute("description", $row['description']);
-		$newnode->setAttribute("datetime", $row['datetime']);
-		$newnode->setAttribute("lat", $row['lat']);
-		$newnode->setAttribute("lng", $row['lng']);
-		$newnode->setAttribute("user_email", $row['email']);
-
-		$admin_id = $row['admin_id'];
-		$query = sprintf("SELECT email FROM users WHERE user_id='$admin_id';");
-		$admin_result = mysqli_query($dbhandle,$query);
-		$admin_row = mysqli_fetch_assoc($admin_result);
-		$newnode->setAttribute("admin_email", $admin_row['email']);
+	
+	while ($page_n<=$page_num){
 		
-		$report_id = $row['report_id'];
-		$query = "SELECT photo_name FROM photos WHERE report_id=$report_id;";
-		$photos_result = mysqli_query($dbhandle,$query);
-		$i = 0;
-		while ($photos_row = mysqli_fetch_assoc($photos_result)){
-			$newnode->setAttribute("photo_name_".$i, $photos_row['photo_name']);
-			$i = $i + 1;
+		$first_report = ($page_n - 1)*20;
+		$query = sprintf("SELECT * FROM (SELECT reports.report_id ,category, description, datetime, lat, lng, email, admin_id FROM reports INNER JOIN status on reports.report_id=status.report_id INNER JOIN users on users.user_id = reports.user_id WHERE status.status ='$status' ORDER BY datetime DESC  LIMIT $first_report,20) AS T1 ORDER BY category;");
+		$result = mysqli_query($dbhandle,$query);
+
+		if (!$result) {  
+			header('HTTP/1.1 500 Error: Could not get reports!'); 
+			exit();
+		} 
+
+		// Iterate through the rows, adding XML nodes for each
+		while ($row = mysqli_fetch_assoc($result)){
+			$node = $dom->createElement("report");
+			$newnode = $parnode->appendChild($node);
+			$newnode->setAttribute("category", $row['category']);
+			$newnode->setAttribute("description", $row['description']);
+			$newnode->setAttribute("datetime", $row['datetime']);
+			$newnode->setAttribute("lat", $row['lat']);
+			$newnode->setAttribute("lng", $row['lng']);
+			$newnode->setAttribute("user_email", $row['email']);
+
+			$admin_id = $row['admin_id'];
+			$query = sprintf("SELECT email FROM users WHERE user_id='$admin_id';");
+			$admin_result = mysqli_query($dbhandle,$query);
+			$admin_row = mysqli_fetch_assoc($admin_result);
+			$newnode->setAttribute("admin_email", $admin_row['email']);
+			
+			$report_id = $row['report_id'];
+			$query = "SELECT photo_name FROM photos WHERE report_id=$report_id;";
+			$photos_result = mysqli_query($dbhandle,$query);
+			$i = 0;
+			while ($photos_row = mysqli_fetch_assoc($photos_result)){
+				$newnode->setAttribute("photo_name_".$i, $photos_row['photo_name']);
+				$i = $i + 1;
+			}
+			$newnode->setAttribute("num_of_photos", $i);
+			$newnode->setAttribute("num_of_reports", $counts);
+			$category = $row['category'];
+			$query = sprintf("SELECT pin_icon FROM categories WHERE category='$category';");
+			$icon_result = mysqli_query($dbhandle,$query);
+			$icon_row = mysqli_fetch_assoc($icon_result);
+			$newnode->setAttribute("pin_icon", $icon_row['pin_icon']);
 		}
-		$newnode->setAttribute("num_of_photos", $i);
-		$newnode->setAttribute("num_of_reports", $counts);
-		$category = $row['category'];
-		$query = sprintf("SELECT pin_icon FROM categories WHERE category='$category';");
-		$icon_result = mysqli_query($dbhandle,$query);
-		$icon_row = mysqli_fetch_assoc($icon_result);
-		$newnode->setAttribute("pin_icon", $icon_row['pin_icon']);
+		$page_n = $page_n + 1 ;
 	}
 
 	if ($counts == 0) {
